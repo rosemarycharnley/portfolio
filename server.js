194ea9949036e8334d576da32543926538a39ad9
rcharnley16@gmail.com <rcharnly@ku.edu>
2023-12-18 13:22:02 -0500

    remove local

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
